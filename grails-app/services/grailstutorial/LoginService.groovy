33dc2dff8ea4107da879b6e34d62c798a9c2f85b
Andrew <drew982@yahoo.com>
2017-01-03 13:29:19 -0600

    Finished C.R.U.D.  (Create, Read, Update, Destroy) Operations

--- grails-app/services/grailstutorial/LoginService.groovy
+++ grails-app/services/grailstutorial/LoginService.groovy
@@ -6,6 +6,7 @@ import grails.transaction.Transactional
 class LoginService //Handles Data Access Objects [CRUD - Create, Read, Update, Destroy]
                     //API - Application Programming Interface - allows for user interaction
 {
+    /****************************READ************************/
     def getUsers()
     {
         User.list()
@@ -24,5 +25,41 @@ class LoginService //Handles Data Access Objects [CRUD - Create, Read, Update, D
     def getHeight()
     {
         User.list().height
+    }
+
+    /***************************CREATE*******************************/
+    def createUser(username, age, height)
+    {
+        User user = new User(username: username, age: age, height: height)
+        user.save()
+    }
+
+    /****************************UPDATE*****************************/
+    def updateUsername(id, username)
+    {
+        User user = User.get(id) //Accesses database location by ID
+        user.username = username //Overrides username field of the User selected by ID
+        user.save() //Checks if object has an ID - if no ID, creates one; if ID, updates
+    }
+
+    def updateAge(id, age)
+    {
+        User user = User.get(id)
+        user.age = age
+        user.save()
+    }
+
+    def updateHeight(id, height)
+    {
+        User user = User.get(id)
+        user.height = height
+        user.save()
+    }
+
+    /******************************DELETE***************************/
+    def deleteByID(id)
+    {
+        User user = User.get(id)
+        user.delete()
     }
 }
